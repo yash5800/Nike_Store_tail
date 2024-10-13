733cfb68a28d10774e63b63d2ba611bc56c54154
A.Yaswanth Kiran <yaswanthjk580@gmail.com>
2024-10-13 18:23:56 +0530

    changes

--- src/sections/Clone.jsx
+++ src/sections/Clone.jsx
@@ -6,8 +6,8 @@ const Clone = () => {
       <a href="https://www.linkedin.com/in/alaparthi-yaswanth-kiran-65aa53272" target='__blank'><img  src={Pic} width={254} className='rounded-full' /></a>
       </div>
       <div className='text-center'>
-         <h3 className='text-4xl text-white font-montserrat font-bold leading-normal'>This <span className='text-coral-red'>Clone</span> Made is <span className='text-blue-500'>By</span></h3>
-         <a href="https://www.linkedin.com/in/alaparthi-yaswanth-kiran-65aa53272" className='text-lg text-slate-gray font-montserrat leading-normal hover:text-white ' target='__blank'>Yaswanth</a>
+         <h3 className='text-4xl text-white font-montserrat font-bold leading-normal'>This <span className='text-coral-red'>Clone</span> is Made <span className='text-blue-500'>By</span></h3>
+         <a href="https://www.linkedin.com/in/alaparthi-yaswanth-kiran-65aa53272" className='text-lg text-slate-gray font-montserrat leading-normal hover:text-white xl:text-xl' target='__blank'>Yaswanth</a>
       </div>
       
     </div>
