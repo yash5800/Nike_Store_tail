b4a82fa1d461010b2691ff71e0ddca7869dc20d7
A.Yaswanth Kiran <yaswanthjk580@gmail.com>
2024-10-13 06:58:15 +0530

    changes

---
+++ src/components/ViewButton.jsx
@@ -0,0 +1,10 @@
+
+const ViewButton = () => {
+  return (
+    <button  className="flex justify-center items-center px-3 py-3.5 border font-montserrat leading-none gap-2 rounded-full text-lg bg-coral-red text-white mt-12">
+    View details
+ </button>
+  )
+}
+
+export default ViewButton
--- src/sections/SuperQuality.jsx
+++ src/sections/SuperQuality.jsx
@@ -1,8 +1,19 @@
-
+import ViewButton from "../components/ViewButton"
+import { shoe8 } from "../assets/images"
+import { useState } from "react"
 const SuperQuality = () => {
   return (
-    <div>
-      
+    <div id="About Us" className="flex xl:flex-row flex-col gap-8 justify-center items-center w-full">
+      <div className="text-white xl:w-2/5 ">
+        <h2 className="text-4xl font-palanquin font-bold lg:max-w-lg">We Provide You <span className="text-coral-red">Super Quality</span> Shoes
+        </h2>
+        <p className="mt-4 font-montserrat leading-normal text-slate-gray lg:max-w-lg">Ensuring premium comfort and style, our meticulously crafted footwear is designed to elevate your experience, providing you with unmatched quality, innovation, and a touch of elegance</p>
+        <p className="mt-6 font-montserrat text-slate-gray lg:max-w-lg">Our dedication to detail and excellence ensures your satisfaction</p>
+         <ViewButton />
+      </div>
+      <div >
+        <img src={shoe8} alt={shoe8} width={570} height={520}  />
+      </div>
     </div>
   )
 }
